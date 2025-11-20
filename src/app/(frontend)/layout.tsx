b90816f21b56672663a0c119dfbb79ed11039db5
Musa Akhmedov <musa.akhmed@pm.me>
2025-11-20 09:11:32 +0100

    frontend page clean

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './styles.css'
 
 export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+  description: 'Espace Culture et Developpement',
+  title: 'Espace Culture et Developpement',
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,15 +1,7 @@
-import { headers as getHeaders } from 'next/headers.js'
-import { getPayload } from 'payload'
-import React from 'react'
-
-import config from '@/payload.config'
 import './styles.css'
 
 export default async function HomePage() {
-  const headers = await getHeaders()
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
+
 
 
   return (
