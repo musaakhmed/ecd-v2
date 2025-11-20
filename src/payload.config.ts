1f1cd3c7480e587be02b0a6a51c1295e0ae0804d
Musa Akhmedov <musa.akhmed@pm.me>
2025-11-20 14:52:56 +0100

    fix type issue in payload.config.ts for s3 storage env data by adding or empty string

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -47,12 +47,12 @@ export default buildConfig({
       },
 
 
-      bucket: process.env.S3_BUCKET,
+      bucket: process.env.S3_BUCKET || '',
       config: {
         forcePathStyle: true,
         credentials: {
-          accessKeyId: process.env.S3_ACCESS_KEY_ID,
-          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+          accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
         },
         region: process.env.S3_REGION,
         endpoint: process.env.S3_ENDPOINT,
