551bb41d02fba832b7ea0c3a68ed22e0c3629a07
Musa Akhmedov <musa.akhmed@pm.me>
2025-11-21 11:06:39 +0100

    add gdpr logo to footer compliance text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { menu } from '@/lib/menu'
+import Image from 'next/image'
 
 const Footer = () => {
   const currentYear = new Date().getFullYear()
@@ -10,7 +11,8 @@ const Footer = () => {
       <div className="container mx-auto px-4 py-12 md:py-16">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-12">
           {/* About Section */}
-          <div className="space-y-4">
+          <div className="hidden lg:block lg:w-1/4"></div>
+          <div className="space-y-4 hidden">
             <h3 className="text-xl font-bold text-white mb-4">Espace Culture et Développement</h3>
             <p className="text-primary-200 text-sm leading-relaxed">
               Espace Cultures & Développement asbl favorise l&apos;intégration et l&apos;insertion
@@ -145,8 +147,9 @@ const Footer = () => {
               </a>
             </div>
 
-            <div>
-              <span className="text-sm">
+            <div className="flex space-x-4 items-center">
+              <Image src="/assets/gdpr.png" alt="GDPR Logo RGPD" height={50} width={50} />
+              <span className="text-xs">
                 Espace Cultures & Développement asbl veille au respect et la protection des données
                 personnelles de ses usages en s&apos;alignant sur le règlement RGPD
               </span>
