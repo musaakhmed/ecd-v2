ec3b8e2e2f8ea0239e47929d4834e40cd7dc51f0
Musa Akhmedov <musa.akhmed@pm.me>
2025-11-24 10:15:16 +0100

    cancel backdrop blur and opacity

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,9 +8,9 @@ import Logo from '@/components/Header/Logo'
 const Header = () => {
   return (
     <motion.header
-      className="h-16 bg-white backdrop-filter backdrop-blur-md  top-0 left-0 sticky flex items-center justify-center shadow-sm z-50 "
-      initial={{ y: -100, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
+      className="h-16 bg-white top-0 left-0 sticky flex items-center justify-center shadow-sm"
+      initial={{ y: -100 }}
+      animate={{ y: 0 }}
       transition={{ duration: 0.5, ease: 'easeOut' }}
     >
       <section className="flex items-center justify-between container mx-auto px-4">
