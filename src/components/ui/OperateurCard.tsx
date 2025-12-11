0cbf4149e2c93573cffe5a64c1a7a06e2ee64a74
Musa Akhmedov <musa.akhmed@pm.me>
2025-12-11 14:08:52 +0100

    add link to object with data

--- src/components/ui/OperateurCard.tsx
+++ src/components/ui/OperateurCard.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import Link from 'next/link'
 
 export function ValueCards() {
   const items = [
@@ -8,21 +9,29 @@ export function ValueCards() {
       icon: '',
       title: 'Opérateur en école de devoirs',
       text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, esse omnis. Consectetur corporis dolores eligendi, excepturi expedita illum, labore magni nobis officiis pariatur porro, quasi sint vel vero voluptate voluptatem!',
+      link: '',
+      img: '',
     },
     {
       icon: '',
       title: "Opérateur d'Economie Sociale",
       text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, esse omnis. Consectetur corporis dolores eligendi, excepturi expedita illum, labore magni nobis officiis pariatur porro, quasi sint vel vero voluptate voluptatem!',
+      link: '',
+      img: '',
     },
     {
       icon: '',
       title: "Opérateur d'EPN labelisé",
       text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, esse omnis. Consectetur corporis dolores eligendi, excepturi expedita illum, labore magni nobis officiis pariatur porro, quasi sint vel vero voluptate voluptatem!',
+      link: '',
+      img: '',
     },
     {
       icon: '',
       title: 'Opérateur de formation de Titres Services',
       text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, esse omnis. Consectetur corporis dolores eligendi, excepturi expedita illum, labore magni nobis officiis pariatur porro, quasi sint vel vero voluptate voluptatem!',
+      link: '',
+      img: '',
     },
   ]
 
@@ -50,10 +59,10 @@ export function ValueCards() {
                 setCardOpen(selectedCard ? null : index)
               }}
             >
-              <div className="relative rounded-xl hover:rounded-b-none bg-white p-6 shadow-md gap-0 transition-all duration-200 ease-in-out cursor-pointer group-hover:cursor-auto">
+              <div className="relative rounded-xl hover:rounded-b-none h-78 bg-white p-6 shadow-md gap-0 transition-all duration-200 ease-in-out cursor-pointer group-hover:cursor-auto">
                 <div className="flex flex-col justify-between gap-2">
                   <div className="flex items-center gap-3">
-                    <span className="inline-flex h-11 w-11 items-center justify-center rounded-full bg-emerald-500 text-white text-lg">
+                    <span className="inline-flex h-11 w-11 items-center justify-center rounded-full bg-emerald-500 text-white text-lg shrink-0">
                       {item.icon}
                     </span>
                     <h3 className="text-sm font-semibold uppercase tracking-wide">{item.title}</h3>
@@ -83,11 +92,17 @@ export function ValueCards() {
                   <div
                     className="absolute left-0 right-0 top-full
                     rounded-xl group-hover:rounded-t-none bg-white p-6 shadow-2xl
-                    opacity-0 -translate-y-5
-                    transition-all duration-300 ease-in-out
-                    group-hover:opacity-100 group-hover:translate-y-0 z-50 flex flex-col gap-6 group-hover:pointer-events-auto"
+                    opacity-0 -translate-y-5 pointer-events-none
+                    transition-all duration-300 ease-in-out z-20
+                    group-hover:opacity-100 group-hover:translate-y-0 flex flex-col gap-6 group-hover:pointer-events-auto"
                   >
                     <p className="text-sm  leading-relaxed">{item.text}</p>
+                    <Link
+                      className="text-secondary-800 hover:text-secondary-900 p-2 px-4 hover:bg-secondary-200 bg-primary-100 rounded-2xl"
+                      href={item.link}
+                    >
+                      Savoir plus
+                    </Link>
                   </div>
                 )}
               </div>
