b2cfea675804abb44e9fa0a52a73c3473e059bb6
Musa Akhmedov <musa.akhmed@pm.me>
2025-12-17 15:41:49 +0100

    remove horizontal scroll section from home page

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import { Hero } from '@/components/homepage/Hero'
 import Projects from '@/components/homepage/Projects'
-import Programs from '@/components/homepage/Programs'
 import Stats from '@/components/homepage/Stats'
 import Partners from '@/components/homepage/Partners'
 import ServiceCards from '@/components/ui/ServiceCards'
@@ -25,7 +24,6 @@ export default async function HomePage() {
       <Video />
       <Stats />
       <Projects />
-      <Programs />
       <Partners />
     </div>
   )
