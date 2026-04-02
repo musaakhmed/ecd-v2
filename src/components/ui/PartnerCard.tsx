ee5e9f4d7e60433fe45eb344ff71e2cdaf66ed99
Musa Akhmedov <musa.akhmed@pm.me>
2026-04-02 10:21:21 +0200

    add link to partners logo on hover text

--- src/components/ui/PartnerCard.tsx
+++ src/components/ui/PartnerCard.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import Image from 'next/image'
 import { partnersContent } from '@/lib/content/partnersAndServicesContent'
 
-type Partners = { title: string; image: string }[]
+type Partners = { title: string; link: string; image: string }[]
 
 const partners: Partners = [...partnersContent.logos]
 
@@ -19,24 +19,27 @@ const PartnerCard = () => {
 
   return (
     <div
-      className="w-full"
+      className="w-full overflow-y-visible! overflow-x-auto"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       style={{ ['--strip-width' as string]: `${STRIP_WIDTH}px` }}
     >
-      <div className="w-full overflow-hidden">
+      <div className="w-full ">
         <div
           className={`flex items-center gap-12 shrink-0 w-max partners-scroll p-2 relative ${isHovered ? 'partners-scroll--paused' : ''}`}
         >
           {[...partners, ...partners].map((partner, idx) => (
             <div
               key={`${partner.title}-${idx}`}
-              className="shrink-0 group flex items-center justify-center transition-transform duration-200 z-50 ease-in-out hover:scale-120 bg-azure-300/30 rounded-xl p-0.5 shadow-md "
+              className="shrink-0 group flex items-center justify-center transition-transform duration-200 z-50 group ease-in-out hover:scale-120  rounded-xl p-0.5 shadow-md "
               style={{ width: CARD_SIZE, height: CARD_SIZE }}
             >
-              <p className="absolute top-0 opacity-0 bg-azure-200 p-0.5 px-1 text-center font-semibold text-primary-900 rounded-lg group-hover:opacity-100 duration-300 transition-all">
-                {partner.title}
+              <p className="absolute z-50 opacity-0 scale-0 w-full bg-azure-200 p-1 text-center font-semibold text-primary-900 rounded-lg group-hover:opacity-100 group-hover:scale-100 duration-300 transition-all">
+                <a href={partner.link} target="blank">
+                  {partner.title}
+                </a>
               </p>
+
               <Image
                 src={partner.image}
                 alt={partner.title}
