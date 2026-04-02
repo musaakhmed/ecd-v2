08f4a6a7baa7e8094b7e23f171ecf207935ae5f5
Musa Akhmedov <musa.akhmed@pm.me>
2026-04-02 11:16:52 +0200

    style: change size, text and margin of the rebranding toaster

--- src/components/RebrandingToast.tsx
+++ src/components/RebrandingToast.tsx
@@ -9,32 +9,26 @@ export function RebrandingToast() {
   useEffect(() => {
     toast(
       (t) => (
-        <section className="flex flex-col justify-between gap-6">
+        <section className="flex flex-col justify-between gap-2">
           <div className="flex justify-between">
-            <h4 className="font-semibold">Vous êtes au bon endroit</h4>
+            <h6 className="font-semibold">Vous êtes au bon endroit</h6>
             <p onClick={() => toast.dismiss(t.id)} className="cursor-pointer text-right text-sm">
               <X className="h-5 w-5" />
             </p>
           </div>
-          <p>
+          <p className="text-xs">
             Nous changeons d’identité visuelle et de nom — auparavant Espace Culture et
-            Développement ASBL, dorénavant <span className="font-bold">Espace Form</span>.
+            Développement ASBL, dorénavant <span className="font-bold">Espace Forma</span>.
           </p>
           <div className="flex gap-4">
-            <Link href="/a-propos/qui-sommes-nous">
-              <button
-                onClick={() => toast.dismiss(t.id)}
-                className="bg-secondary-50 py-1.5 px-3 rounded text-primary-800 cursor-pointer"
-              >
-                Savoir plus
-              </button>
-            </Link>
-            <button
-              onClick={() => toast.dismiss(t.id)}
-              className="flex items-center gap-2 bg-secondary-50/5 py-1.5 px-4 rounded text-primary-100 border border-primary-100 cursor-pointer hover"
-            >
-              Continuer <ArrowRight size={16} strokeWidth={2} />
-            </button>
+            {/*<Link href="/a-propos/qui-sommes-nous">*/}
+            {/*  <button*/}
+            {/*    onClick={() => toast.dismiss(t.id)}*/}
+            {/*    className="bg-secondary-50 py-1.5 px-3 rounded text-primary-800 cursor-pointer"*/}
+            {/*  >*/}
+            {/*    Savoir plus*/}
+            {/*  </button>*/}
+            {/*</Link>*/}
           </div>
         </section>
       ),
@@ -50,6 +44,8 @@ export function RebrandingToast() {
           color: 'var(--color-primary-50)',
           border: '1px solid var(--color-primary-600)',
           boxShadow: '0 18px 38px rgba(13, 28, 38, 0.28)',
+          margin: '4rem',
+          marginTop: '6rem',
           backgroundImage:
             'radial-gradient(120px 120px at 95% 5%, rgba(53, 113, 151, 0.45) 0%, rgba(53, 113, 151, 0) 65%), linear-gradient(135deg, var(--color-primary-600) 0%, var(--color-primary-700) 100%)',
         },
