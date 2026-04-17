0528623b12b6ca18400d70273fb160961019e5c9
Musa Akhmedov <musa.akhmed@pm.me>
2026-04-17 11:37:12 +0200

    fix: restaure incorrect changes from previous commit

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -111,7 +111,7 @@ const Page = () => {
   return (
     <div className="min-h-screen bg-linear-to-b from-white to-primary-50">
       {/* Hero Section */}
-      <section className="relative h-[35vh] flex items-center bg-white text-white overflow-hidden">
+      <section className="relative h-[35vh] flex items-center bg-linear-to-r from-primary-600 to-secondary-600 text-white overflow-hidden">
         <div className="absolute inset-0 bg-black/10" />
         <div className="container mx-auto px-4 relative z-10 py-6">
           <motion.div
