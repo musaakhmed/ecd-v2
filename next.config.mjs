2ced542417fd89a7e79480fcde68cbd4140010f4
Musa Akhmedov <musa.akhmed@pm.me>
2026-08-25 13:39:29 +0200

    feat: add redirection links to evaluation forms

--- next.config.mjs
+++ next.config.mjs
@@ -27,6 +27,26 @@ const nextConfig = {
         destination: '/inclusion-numerique/seniors-digital',
         permanent: true,
       },
+      {
+        source: '/quiz1a',
+        destination: 'https://forms.gle/mWDtJjMS9h8ncD7u6',
+        permanent: true,
+      },
+      {
+        source: '/quiz1b',
+        destination: 'https://forms.gle/7fc4Ccgo4xdWHo5n7',
+        permanent: true,
+      },
+      {
+        source: '/quiz2',
+        destination: 'https://forms.gle/oJsFa9afWVGsAX3XA',
+        permanent: true,
+      },
+      {
+        source: '/evaluation-ts',
+        destination: 'https://forms.gle/rfCioBKNw4eA7FSW9',
+        permanent: true,
+      },
     ]
   },
   // Your Next.js config here
